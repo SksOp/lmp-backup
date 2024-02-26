37684923000721e50db27bdbaaba2af2265c40b1
SksOp <shubhaman47@gmail.com>
2024-02-26 17:05:02 +0530

    updated designs

--- src/components/application.tsx
+++ src/components/application.tsx
@@ -1,5 +1,4 @@
-
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -7,44 +6,52 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { Label } from "@/components/ui/label"
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
 import { Icon } from "@iconify/react";
 import { LeadLogo } from "./leadLogo";
 import DocUpload from "./docUpload";
 import { MinimalLead } from "@/types";
 
-export function Application( {lead, btns}: {lead: MinimalLead, btns:React.ReactNode} ) {
+export function Application({
+  lead,
+  btns,
+}: {
+  lead: MinimalLead;
+  btns: React.ReactNode;
+}) {
   return (
+    <Card>
+      <CardHeader className="flex flex-row gap-3 items-center">
+        <LeadLogo imageName={lead.lead_name} />
+        <div>
+          <CardTitle className="text-md">{lead.lead_name}</CardTitle>
+          <div className="flex items-center opacity-60 gap-2">
+            <Icon
+              icon="streamline:user-identifier-card"
+              className=" w-4 h-4 text-black-500"
+            />
 
-    <Card className="w-full border-r-2">
-      <CardHeader className="flex flex-row gap-3">
-        <LeadLogo imageName={lead.lead_name}/>
-        <div className="justify-item-center">
-        <CardTitle>{lead.lead_name}</CardTitle>
-        <CardDescription className="flex gap-3">
-          
-          <Icon icon="streamline:user-identifier-card" className="mt-3 w-4 h-4 text-black-500" />
-          
-          <p className="py-2">{lead.lead_id}</p>
-        </CardDescription>
+            <p>{lead.lead_id}</p>
+          </div>
         </div>
         <div className="flex-grow"></div>
-        <Icon icon="ep:arrow-right" className="h-8 w-8 text-black-500" />
+        <Icon icon="ep:arrow-right" className="h-4 w-4 text-black-500" />
       </CardHeader>
       <CardContent>
         <div className="flex justify-between">
           <div className="flex flex-col gap-1">
-          <h3 className="text-bold">Leasing Status:</h3>
-          <p>{lead.current_state.label.en}</p>
+            <h3 className="text-bold">Leasing Status:</h3>
+            <p>{lead.current_state.label.en}</p>
           </div>
-          <h3 >Applied: {lead.created_date[8]+lead.created_date[9]}/{lead.created_date[5]+lead.created_date[6]}/{lead.created_date.substring(0,4)}</h3>
-          
+          <h3>
+            Applied: {lead.created_date[8] + lead.created_date[9]}/
+            {lead.created_date[5] + lead.created_date[6]}/
+            {lead.created_date.substring(0, 4)}
+          </h3>
         </div>
       </CardContent>
-      <CardFooter className="flex gap-3">
-      {btns}
-      </CardFooter>
+      <CardFooter className="flex gap-3">{btns}</CardFooter>
     </Card>
-  )
+  );
 }
--- src/components/navBottom.tsx
+++ src/components/navBottom.tsx
@@ -3,13 +3,13 @@ import React from "react";
 
 function NavBottom() {
   return (
-    <nav className="w-full h-14 bg-background flex justify-between items-center z-10 px-3 py-5 sticky bottom-0 border-t-2">
+    <nav className="w-full h-14 bg-background flex justify-between items-center z-10 px-3 py-5 fixed bottom-0 border-t-2">
       <a href="/">
         <Icon icon="solar:home-2-bold" className="h-8 w-8" />
       </a>
 
       <a href="/">
-      <Icon icon="zondicons:add-solid" className="h-14 w-14 mb-4" />
+        <Icon icon="zondicons:add-solid" className="h-14 w-14 mb-4" />
       </a>
       <a href="/">
         <Icon icon="fluent:people-20-regular" className="h-7 w-7" />
