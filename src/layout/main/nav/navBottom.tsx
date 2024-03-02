102a0f441a455e5e0fa7b037deae09d7677f169c
SksOp <shubhaman47@gmail.com>
2024-03-02 13:52:54 +0530

    Update shadow color in navBottom component

--- src/layout/main/nav/navBottom.tsx
+++ src/layout/main/nav/navBottom.tsx
@@ -3,7 +3,7 @@ import { Icon } from "@iconify/react";
 import { Link } from "react-router-dom";
 function NavBottom() {
   return (
-    <nav className="w-full h-14 bg-background flex justify-between  items-center z-10 px-3 py-5 fixed bottom-0  shadow-[0_-8px_15px_-3px_rgba(0,0,0,0.2)] border-t-2">
+    <nav className="w-full h-14 bg-background flex justify-between  items-center z-10 px-3 py-5 fixed bottom-0  shadow-[0_-8px_15px_-8px_rgba(0,0,0,0.2)] border-t-2">
       <Link to={paths.home}>
         <Icon icon="solar:home-2-bold" className="h-8 w-8" />
       </Link>
