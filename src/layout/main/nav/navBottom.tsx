ffab6ed3586d86f4aff65a3d908390787e99f467
SksOp <shubhaman47@gmail.com>
2024-03-02 13:52:31 +0530

    Update NavBottom component styling

--- src/layout/main/nav/navBottom.tsx
+++ src/layout/main/nav/navBottom.tsx
@@ -1,17 +1,16 @@
 import { paths } from "@/router";
 import { Icon } from "@iconify/react";
 import { Link } from "react-router-dom";
-
 function NavBottom() {
   return (
-    <nav className="w-full h-14 bg-background flex justify-between items-center z-10 px-3 py-5 fixed bottom-0 border-t-2">
+    <nav className="w-full h-14 bg-background flex justify-between  items-center z-10 px-3 py-5 fixed bottom-0  shadow-[0_-8px_15px_-3px_rgba(0,0,0,0.2)] border-t-2">
       <Link to={paths.home}>
         <Icon icon="solar:home-2-bold" className="h-8 w-8" />
       </Link>
 
-      <a href={paths.application}>
+      <Link to={paths.application}>
         <Icon icon="zondicons:add-solid" className="h-14 w-14 mb-4" />
-      </a>
+      </Link>
       <Link to={paths.allLead}>
         <Icon icon="fluent:people-20-regular" className="h-7 w-7" />
       </Link>
