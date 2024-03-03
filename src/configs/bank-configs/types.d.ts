85fc98ca346330361cb95d9fb42c8ddf8dad0637
SksOp <shubhaman47@gmail.com>
2024-03-03 11:36:05 +0530

    Add BankConfig interface with input field definitions

--- src/configs/bank-configs/types.d.ts
+++ src/configs/bank-configs/types.d.ts
@@ -18,3 +18,51 @@ export interface BankConfig {
     [key: string]: any;
   }[];
 }
+
+export interface BankConfig {
+  state_id: number | string;
+  state_name: {
+    en: string;
+    ar: string;
+  };
+  state_type: string;
+  actor: string;
+  input_fields: {
+    input_field_id: string; //customer_iqama_id
+
+    label: {
+      en: string;
+      ar: string;
+    };
+    description: {
+      en: string;
+      ar: string;
+    };
+    rank: number;
+    group: number;
+    group_name: {
+      en: string;
+      ar: string;
+    };
+    group_description: {
+      en: string;
+      ar: string;
+    };
+    validationRegex?: string;
+    verificationRequired?: boolean;
+    apiValidation?: {
+      initiateEndpoint: string;
+      verifyEndpoint: string;
+      expectedResponse: string;
+    };
+    input_field_type: "radio" | "radio_multimedia" | "phone" | "text" | "number" | "date" | "file";
+    options?: {
+      image_url: string;
+      label: {
+        en: string;
+        ar: string;
+      };
+    };
+    dependacy: number; //TODO
+  }[];
+}
