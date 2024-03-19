32010cc3cbbbb9da7a9fa67b637a533a606dac92
Ayush Kaithwas <ayush.91011@gmail.com>
2024-03-19 05:39:50 +0530

    Add line chart component and update bottom navigation

--- src/components/leadsFunnel.tsx
+++ src/components/leadsFunnel.tsx
@@ -7,7 +7,11 @@ import { paths } from "@/router";
 import { Card, CardContent, CardHeader } from "./ui/card";
 import { cn } from "@/lib/utils";
 
-Chart.register(ArcElement, Tooltip, Legend, Title);
+import { Line } from "react-chartjs-2";
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement } from "chart.js";
+
+// Registering the components needed for the line chart
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
 const data = {
   labels: ["Initiated", "Bank Verifications", "Document Verification", "Dealer Acknowledges", "Amount Disbursed", "Loan Rejected"],
@@ -19,6 +23,27 @@ const data = {
     },
   ],
 };
+
+const SimpleBarChart: React.FC = () => {
+  // Calculate total value for percentage calculation
+  const total = data.datasets[0].data.reduce((acc, point) => acc + point, 0);
+
+  return (
+    <div className="flex gap-1 w-[100%]">
+      {data.labels.map((label, index) => {
+        const value = data.datasets[0].data[index];
+        const backgroundColor = data.datasets[0].backgroundColor[index];
+        if (index === 0)
+          return <div className="rounded-l-full" style={{ width: `${(value / total) * 100}%`, backgroundColor: backgroundColor, height: "10px" }} />;
+        if (index === data.labels.length - 1)
+          return <div className="rounded-r-full" style={{ width: `${(value / total) * 100}%`, backgroundColor: backgroundColor, height: "10px" }} />;
+        return <div className="" style={{ width: `${(value / total) * 100}%`, backgroundColor: backgroundColor, height: "10px" }} />;
+      })}
+    </div>
+  );
+};
+
+Chart.register(ArcElement, Tooltip, Legend, Title);
 
 const options = {
   cutout: "80%", // Adjust this value to control the thickness of the doughnut
@@ -33,30 +58,31 @@ const options = {
 function LeadsFunnel() {
   const total = data.datasets[0].data.reduce((a, b) => a + b, 0);
   return (
-    <Card className="bg-none w-full">
-      <CardContent className="p-6">
-        <div className="flex flex-col justify-between items-start">
+    <Card className="bg-none w-full md:border-0 shadow-none">
+      <CardContent className="flex flex-col px-4 py-5 gap-4">
+        <div className="flex flex-col md:flex-row justify-between md:justify-normal gap-2 items-start md:items-center">
           <h2>
             <span className="font-bold">{total}</span> <span className="font-medium">Total Applications:</span>
           </h2>
-          <ul className="flex flex-wrap gap-1">
+          <ul className="flex flex-wrap gap-3">
             {data.labels.map((label, index) => (
-              <li key={index} className="flex justify-center text-xl my-2 items-center">
+              <li key={index} className="flex md:gap-1 justify-center text-xl items-center">
                 <span
-                  className="text-sm"
+                  className="text-xs items-center justify-center"
                   style={{
                     color: data.datasets[0].backgroundColor[index],
                   }}
                 >
                   ⬤
                 </span>
-                <span className="font-bold sm:text-lg min-[500px]:text-base text-xs ml-2">
+                <span className="font-bold text-sm ml-1">
                   {data.datasets[0].data[index]} <span className="font-normal ">{label}</span>
                 </span>
               </li>
             ))}
           </ul>
         </div>
+        <SimpleBarChart />
       </CardContent>
     </Card>
   );
--- src/layout/main/nav/navBottom.tsx
+++ src/layout/main/nav/navBottom.tsx
@@ -11,13 +11,39 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "@/components/ui/drawer";
+import { Plus } from "lucide-react";
+import { useEffect, useState } from "react";
 
 function NavBottom() {
+  const [scrollDirection, setScrollDirection] = useState("down");
+  const [lastScrollTop, setLastScrollTop] = useState(0);
+
+  useEffect(() => {
+    const onScroll = () => {
+      const currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      if (currentScrollTop > lastScrollTop) {
+        setScrollDirection("down");
+      } else if (currentScrollTop < lastScrollTop) {
+        setScrollDirection("up");
+      }
+      setLastScrollTop(currentScrollTop <= 0 ? 0 : currentScrollTop); // For Mobile or negative scrolling
+    };
+
+    window.addEventListener("scroll", onScroll, { passive: true });
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [lastScrollTop]);
   return (
-    <footer className="z-10 fixed bottom-0 right-0 m-5">
+    <footer className="z-10 fixed bottom-0 right-0 m-7">
       <Drawer>
         <DrawerTrigger>
-          <EditIcon className="" />
+          {scrollDirection === "down" && <Plus className=" bg-primary text-white rounded-full w-16 h-16 p-4" />}
+          {scrollDirection === "up" && (
+            <div className="flex items-center text-white rounded-full pl-4 pr-5 py-3 justify-center bg-primary">
+              <Plus className=" bg-primary  rounded-full w-10 h-10 pr-2" />
+              <h2 className="font-semibold">Apply</h2>
+            </div>
+          )}
         </DrawerTrigger>
         <DrawerContent className="min-h-[60vh]">
           <ApplicationDrawerContent />
