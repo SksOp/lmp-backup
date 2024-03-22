6206f17935eed5931b5a5c029a5fee90a36a9e22
Ankit-69k <ankitkumarray19@gmail.com>
2024-03-22 14:21:15 +0530

    fixed application navbar

--- src/layout/application/nav/navbar.tsx
+++ src/layout/application/nav/navbar.tsx
@@ -6,13 +6,15 @@ import { Link } from 'react-router-dom'
 function Navbar() {
   return (
         
-        <nav className="w-full  flex justify-center items-center bg-background z-10 px-3 py-5 fixed top-0 border-b">
-        <div className="absolute left-4 flex items-center gap-1">
+    <nav className="w-full bg-background flex justify-between items-center z-10 px-4 py-3 sticky top-0 border-b-2">
+        <div className="flex items-center gap-1">
           <Link to={paths.home}>
           <BackIcon />
           </Link>
         </div>
-        <h3 className="text-s font-bold">New Leasing Application</h3>
+        <div className="flex-grow flex justify-center">
+    <h3 className="text-sm font-bold">New Leasing Application</h3>
+  </div>
       </nav>
   )
 }
